2443a7df024c1ffe61d7edb050f24e7e8b5d4220
Dhivshnika <dhivshnikaselvaraj002@gmail.com>
2024-01-23 13:56:06 +0530

    added

--- form_validation/src/screens/login/login.js
+++ form_validation/src/screens/login/login.js
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router-dom';
 function Login() {
     const [email, setEmail] = useState('');
     const [pwd, setPwd] = useState('');
+    const [emailValid, setEmailValid] = useState(true);
+    const [pwdValid, setPwdValid] = useState(true);
     const navigate = useNavigate();
     const handleEmailChange = (e) => {
         const inputValue = e.target.value;
@@ -17,34 +19,16 @@ function Login() {
         setPwd(inputValue);
     };
     const handleLogin = () => {
-        const symbol = email.indexOf('@');
-        const dot = email.indexOf('.');
-        let count1 = 0, count2 = 0, count3 = 0, count4 = 0;
-        for (let i = 0; i < pwd.length; i++) {
-            if (pwd.charAt(i) >= 'a' && pwd.charAt(i) <= 'z') {
-                count1++;
-            }
-            if (pwd.charAt(i) >= 'A' && pwd.charAt(i) <= 'Z') {
-                count2++;
-            }
-            if (pwd.charAt(i) >= '0' && pwd.charAt(i) <= '9') {
-                count3++;
-            }
-            else {
-                count4++;
-            }
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+        const isEmailValid = emailRegex.test(email);
+        const isPwdValid = pwdRegex.test(pwd);
+        setEmailValid(isEmailValid);
+        setPwdValid(isPwdValid);
+        if (emailValid && pwd != "" && pwdValid) {
+            navigate('/form');
         }
-        if (symbol < 1 || dot < symbol + 2 || dot + 2 >= email.length) {
-            alert('Enter valid mail Id');
-        }
-        else {
-            if (pwd.length < 8 || count1 < 1 || count2 < 1 || count3 < 1 || count4 < 1) {
-                alert("Password must be 8 digits and should contain atleast one capital letter,small letter,digits and special character");
-            }
-            else {
-                navigate('/form');
-            }
-        }
+        console.log(emailValid + " " + pwdValid);
     };
     return (
         <div className='mainContainer'>
@@ -55,11 +39,21 @@ function Login() {
                     <input type='mail' required placeholder='User Mail' value={email}
                         onChange={handleEmailChange} />
                 </div>
+                {emailValid ? null :
+                    (
+                        <div className='error'>Enter valid mail Id</div>
+                    )
+                }
                 <div className='passwordBox'>
                     <img src={pwds} alt='mail' height='20' width='20' />
                     <input type='password' required placeholder='Password' value={pwd}
                         onChange={handlePwdChange} />
                 </div>
+                {pwdValid ? null :
+                    (
+                        <div className='error'>Must contain at least 8 characters, one letter,one special character and one number</div>
+                    )
+                }
                 <div className='button' onClick={handleLogin}>Login Now</div>
             </div>
             <div className='designBox'>
